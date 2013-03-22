40aac51fdb1d978f81475e8f3820fd0fc4fbe237
Gordon Fontenot <gordon@thoughtbot.com>
2013-03-22 15:30:50 -0400

    Add keyboard shortcut

--- spectacular/Classes/TBSpectacularPlugin.m
+++ spectacular/Classes/TBSpectacularPlugin.m
@@ -8,11 +8,18 @@
 
 #import "TBSpectacularPlugin.h"
 
+@interface TBSpectacularPlugin ()
+
+@property (nonatomic, strong) NSMenuItem *specMenuItem;
+
+@end
+
 @implementation TBSpectacularPlugin
 
 static TBSpectacularPlugin *spectacularPlugin = nil;
 
-+ (void)pluginDidLoad:(NSBundle *)plugin {
++ (void)pluginDidLoad:(NSBundle *)plugin
+{
     static dispatch_once_t onceToken;
     dispatch_once(&onceToken, ^{
         spectacularPlugin = [[self alloc] init];
@@ -22,6 +29,47 @@ static TBSpectacularPlugin *spectacularPlugin = nil;
 + (TBSpectacularPlugin *)sharedPlugin
 {
     return spectacularPlugin;
+}
+
+- (id)init
+{
+    self = [super init];
+    if (!self) return nil;
+
+    [self.specMenuItem setTitle:@"Jump to test file"];
+    return self;
+
+}
+
+- (void)addMenuItems
+{
+}
+
+- (void)jumpToSpec
+{
+    [self.specMenuItem setTitle:@"Howdy!!!"];
+}
+
+- (BOOL)validateMenuItem:(NSMenuItem *)menuItem
+{
+    return YES;
+}
+
+#pragma mark - Getters
+
+- (NSMenuItem *)specMenuItem
+{
+    if (!_specMenuItem) {
+        NSMenu *mainMenu = [NSApp mainMenu];
+        
+        NSMenuItem *navMenu = [mainMenu itemWithTitle:@"Edit"];
+        _specMenuItem = [[NSMenuItem alloc] initWithTitle:@"" action:@selector(jumpToSpec) keyEquivalent:@"t"];
+        [_specMenuItem setKeyEquivalentModifierMask:NSControlKeyMask | NSAlternateKeyMask | NSCommandKeyMask];
+        [_specMenuItem setTarget:self];
+        [[navMenu submenu] addItem:_specMenuItem];
+    }
+
+    return _specMenuItem;
 }
 
 @end
