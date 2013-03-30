067e1b3845ed6dd9cc4f0c33b7c42e68bddedaa2
Gordon Fontenot <gordon@thoughtbot.com>
2013-03-29 23:31:04 -0400

    Clean up private headers

--- spectacular/Headers/Xcode.h
+++ spectacular/Headers/Xcode.h
@@ -1,73 +1,47 @@
 //Headers dumped from IDEKit, IDEFoundation, DVTKit, and DVTFoundation
 
+@interface IDEIndexCollection : NSObject
+
+- (id)firstObject;
+
+@end
+
 @interface IDEIndex : NSObject
 
-//+ (id)resolutionForName:(id)arg1 kind:(id)arg2 containerName:(id)arg3;
-- (id)filesContaining:(id)arg1 anchorStart:(BOOL)arg2 anchorEnd:(BOOL)arg3 subsequence:(BOOL)arg4 ignoreCase:(BOOL)arg5 cancelWhen:(id)arg6;
+- (IDEIndexCollection *)filesContaining:(NSString *)string anchorStart:(BOOL)anchorStart anchorEnd:(BOOL)anchorEnd subsequence:(BOOL)subsequence ignoreCase:(BOOL)ignoreCase cancelWhen:(id)arg6;
 
 @end
 
 @interface IDEWorkspace : NSObject
 
 @property(retain) IDEIndex *index;
-//
-//- (void)_updateIndexableFiles:(id)arg1;
-//
-@end
-
-@interface IDEIndexCollection : NSObject
-
-- (id)firstObject;
-//- (id)allObjects;
 
 @end
-
-//@interface IDEEditor : NSObject
-//
-//@end
 
 @interface IDENavigableItem : NSObject
-
-@end
-
-@interface IDEDocumentURLNavigableItem : IDENavigableItem
-
-- (id)initWithRepresentedObject:(id)arg1;
-
-//- (id)fileURL;
-//- (id)contentDocumentLocation;
-//- (id)documentType;
-//- (id)name;
-//- (id)newImage;
-//- (id)_documentURL;
 
 @end
 
 @interface IDENavigableItemCoordinator : NSObject
 
 - (void)registerNavigableItem:(IDENavigableItem *)navigableItem;
-- (id)structureNavigableItemForDocumentURL:(id)arg1 inWorkspace:(id)arg2 error:(id *)arg3;
-
-@end
-
-@interface IDEEditorContext : NSObject
-
-@property(retain, nonatomic) IDENavigableItem *navigableItem; // @synthesize navigableItem=_navigableItem;
-
-@property(readonly) IDENavigableItemCoordinator *navigableItemCoordinator; // @synthesize navigableItemCoordinator=_navigableItemCoordinator;
-//- (void)setNavBarNavigableItem:(id)arg1;
-
-//- (int)_openNavigableItem:(id)arg1 withContentsOfURL:(id)arg2 shouldInstallEditorBlock:(id)arg3;
-//- (void)_navigateAwayFromCurrentDocumentWithURL:(id)arg1;
-- (id)currentHistoryItem;
-
-- (IDEWorkspace *)workspace;
+- (IDENavigableItem *)structureNavigableItemForDocumentURL:(NSURL *)documentURL inWorkspace:(IDEWorkspace *)workspace error:(NSError *)error;
 
 @end
 
 @interface IDEEditorHistoryItem : NSObject
 
-@property(readonly) NSURL *documentURL;
+@property (readonly) NSURL *documentURL;
+
+@end
+
+@interface IDEEditorContext : NSObject
+
+@property (retain, nonatomic) IDENavigableItem *navigableItem;
+@property (readonly) IDENavigableItemCoordinator *navigableItemCoordinator;
+
+- (IDEEditorHistoryItem *)currentHistoryItem;
+- (IDEWorkspace *)workspace;
 
 @end
 
@@ -76,4 +50,3 @@
 @property (readonly) NSURL *fileURL;
 
 @end
-
